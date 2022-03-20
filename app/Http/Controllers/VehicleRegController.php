888ad35a16b798536e5d14e0ebecaa30b6190bd9
shawn mazhude <shawnmazhude@gmail.com>
2022-03-21 00:50:22 +0200

    login done

---
+++ app/Http/Controllers/VehicleRegController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VehicleRegController extends Controller
+{
+    //
+    function index()
+    {
+        return "vehicle registration details";
+    }
+}
